0ba180ee8cd60a7a3011987e036593d43bd9d6ca
twinh <twinhuang@qq.com>
2018-01-12 16:07:51 +0800

    beforePageScript 合并到 script 中

--- src/Plugin.php
+++ src/Plugin.php
@@ -6,7 +6,7 @@ class Plugin extends \Miaoxing\Plugin\BasePlugin
 {
     protected $name = 'Ueditor PHP后端';
 
-    public function onBeforePageScript()
+    public function onScript()
     {
         $controller = $this->app->getController();
         if (strpos($controller, 'admin') !== false) {
