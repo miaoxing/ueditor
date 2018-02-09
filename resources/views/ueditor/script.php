29fbbc822b7469d7f954e7d2646ad300d0afd9ea
twinh <twinhuang@qq.com>
2018-02-09 10:02:11 +0800

    更新 ueditor 的配置到插件中

--- resources/views/ueditor/script.php
+++ resources/views/ueditor/script.php
@@ -1,7 +1,20 @@
 <script>
   require.config({
+    paths: {
+      ueditor: 'plugins/ueditor/libs/neditor/i18n/zh-cn/zh-cn',
+      'ueditor-all': 'plugins/ueditor/libs/neditor/neditor.all.min',
+      'ueditor-config': 'plugins/ueditor/js/neditor.config',
+    },
+    shim: {
+      ueditor: {
+        deps: ['ueditor-all']
+      },
+      'ueditor-all': {
+        deps: ['ueditor-config']
+      }
+    },
     config: {
-      'ueditor.config': <?= $wei->ueditor->getConfigJson() ?>
+      'ueditor-config': <?= $wei->ueditor->getConfigJson() ?>
     }
   });
 </script>
