c0b744d7f5ae27b2a9163aade8b32ba9464ca5ad
twinh <twinhuang@qq.com>
2019-02-19 18:29:25 +0800

    refactoring: spacing 更新

--- public/libs/neditor/dialogs/music/music.js
+++ public/libs/neditor/dialogs/music/music.js
@@ -155,12 +155,12 @@ function Music() {
                     t.push('<div id="page' + i + '" onclick="music.onpageclick(' + i + ')" class="pageoff">' + (i ) + '</div>');
                 }
                 s.push('<div class="m-box">');
-                s.push('<div class="m-h"><span class="m-t">' + lang.chapter + '</span><span class="m-s">' + lang.singer
+                s.push('<div class="m-h"><span class="mt-3">' + lang.chapter + '</span><span class="m-s">' + lang.singer
                     + '</span><span class="m-z">' + lang.special + '</span><span class="m-try-t">' + lang.listenTest + '</span></div>');
                 for (var j = 0, tmpObj; tmpObj = tmpList[j++];) {
                     s.push('<label for="radio-' + i + '-' + j + '" class="m-m">');
-                    s.push('<input type="radio" id="radio-' + i + '-' + j + '" name="musicId" class="m-l" onclick="music.doselect(' + (me.pageSize * (i-1) + (j-1)) + ')"/>');
-                    s.push('<span class="m-t">' + me._getMaxText(tmpObj.title) + '</span>');
+                    s.push('<input type="radio" id="radio-' + i + '-' + j + '" name="musicId" class="ml-3" onclick="music.doselect(' + (me.pageSize * (i-1) + (j-1)) + ')"/>');
+                    s.push('<span class="mt-3">' + me._getMaxText(tmpObj.title) + '</span>');
                     s.push('<span class="m-s">' + me._getMaxText(tmpObj.author) + '</span>');
                     s.push('<span class="m-z">' + me._getMaxText(tmpObj.album_title) + '</span>');
                     s.push('<span class="m-try" onclick="music.doselect(' + (me.pageSize * (i-1) + (j-1)) + ');music.listenTest(this)"></span>');
