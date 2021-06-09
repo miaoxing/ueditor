98af651819b79c6c482fa47791bdd342e1963eb3
twinh <twinhuang@qq.com>
2021-06-09 22:42:45 +0800

    fix(ueditor): ueditor 上传文件是 POST 表单，不支持 JWT，改为 Session 登录

--- src/UeditorPlugin.php
+++ src/UeditorPlugin.php
@@ -2,9 +2,23 @@
 
 namespace Miaoxing\Ueditor;
 
+use Miaoxing\Plugin\Auth\SessionAuth;
 use Miaoxing\Plugin\BasePlugin;
 
+/**
+ * @mixin \AppMixin
+ */
 class UeditorPlugin extends BasePlugin
 {
     protected $name = 'Ueditor 富文本编辑器';
+
+    public function onAppInit()
+    {
+        // TODO ueditor 上传文件是 POST 表单，不支持 JWT，改为 Session 登录
+        if ('/ueditor' === $this->req->getPathInfo()) {
+            wei()->setConfig('user', [
+                'authClass' => SessionAuth::class,
+            ]);
+        }
+    }
 }
